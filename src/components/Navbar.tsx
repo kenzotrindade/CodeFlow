c5e0a5a0948eb6fb9df1976554b2caff2346197c
kenzotrindade <kenzo.trindade@gmail.com>
2026-05-18 09:22:50 +0200

    Edit: Navbar with session

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,28 +1,47 @@
 import Link from "next/link";
+import { auth } from "@/lib/auth";
 
 // #################################
 // ### Navbar Component
 // #################################
 
-export default function Navbar() {
+export default async function Navbar() {
+  const session = await auth();
   return (
     <div>
       <nav className="flex justify-between p-6 text-2xl bg-gray-800">
         <Link href={"/"} className="text-white">
           CodeFlow.
         </Link>
-        <ul className="flex gap-6 text-blue-500 underline">
-          <li>
-            <Link href="/login" className="hover:text-blue-400">
-              Login
-            </Link>
-          </li>
-          <li>
-            <Link href="/register" className="hover:text-blue-400">
-              Register
-            </Link>
-          </li>
-        </ul>
+        <div>
+          {session ? (
+            <ul className="flex gap-6 text-blue-500 underline">
+              <li>
+                <Link href="/dashboard" className="hover:text-blue-400">
+                  Dashboard
+                </Link>
+              </li>
+              <li>
+                <Link href="/account" className="hover:text-blue-400">
+                  Account
+                </Link>
+              </li>
+            </ul>
+          ) : (
+            <ul className="flex gap-6 text-blue-500 underline">
+              <li>
+                <Link href="/login" className="hover:text-blue-400">
+                  Login
+                </Link>
+              </li>
+              <li>
+                <Link href="/register" className="hover:text-blue-400">
+                  Register
+                </Link>
+              </li>
+            </ul>
+          )}
+        </div>
       </nav>
     </div>
   );
