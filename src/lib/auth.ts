3675785098df95e50a213c161349363c1adc3fcb
kenzotrindade <kenzo.trindade@gmail.com>
2026-06-15 14:21:49 +0200

    Test prod

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -9,6 +9,9 @@ import bcrypt from "bcryptjs";
 // ### Auth Settings
 // #################################
 
+console.log("AUTH_SECRET:", process.env.AUTH_SECRET);
+console.log("NEXTAUTH_SECRET:", process.env.NEXTAUTH_SECRET);
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   secret: process.env.AUTH_SECRET || process.env.NEXTAUTH_SECRET,
   adapter: PrismaAdapter(prisma),
