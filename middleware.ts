56fb9a75203a992ac4ee6381472814227f6d2dc0
kenzotrindade <kenzo.trindade@gmail.com>
2026-06-15 12:20:34 +0200

    Deploy vercel

--- middleware.ts
+++ middleware.ts
@@ -6,6 +6,8 @@ import { NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 import type { NextRequest } from "next/server";
 
+export const runtime = 'nodejs';
+
 export async function middleware(req: NextRequest) {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
