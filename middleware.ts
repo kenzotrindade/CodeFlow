944f10cb5ce99cd0cded672b50e3c598640f5d9a
kenzotrindade <kenzo.trindade@gmail.com>
2026-06-15 13:10:46 +0200

    Vercel

--- middleware.ts
+++ middleware.ts
@@ -2,21 +2,8 @@
 // ### Middleware
 // #################################
 
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-
-export function middleware(req: NextRequest) {
-  const sessionToken =
-    req.cookies.get("next-auth.session-token") ||
-    req.cookies.get("__Secure-next-auth.session-token");
-
-  if (!sessionToken) {
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
-
-  return NextResponse.next();
-}
-
 export const config = {
   matcher: ["/dashboard", "/techwatch", "/account", "/exercise/:path*"],
 };
+
+export { auth as default } from "@/lib/auth";
